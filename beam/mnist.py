e1ec4c31cc1618ad5cf98950de78b410fda7bd76
Anna Chen <annachen@Annas-MacBook-Pro-2.local>
2018-10-06 15:41:22 -0700

    finalize non-batch version

--- beam/mnist.py
+++ beam/mnist.py
@@ -17,13 +17,13 @@ rbm_params = {
 def train_mnist(batch_size=1, learning_rate=0.1):
     # prepare data
     trainset = mnist.MNIST(
-        root=MNIST_ROOT, 
-        train=True, 
-        download=True, 
+        root=MNIST_ROOT,
+        train=True,
+        download=True,
         transform=transforms.ToTensor())
     testset = mnist.MNIST(
-        root=MNIST_ROOT, 
-        train=False, 
+        root=MNIST_ROOT,
+        train=False,
         transform=transforms.ToTensor())
 
     train_iter = torch.utils.data.DataLoader(
@@ -32,9 +32,10 @@ def train_mnist(batch_size=1, learning_rate=0.1):
         testset, batch_size=batch_size, shuffle=True, num_workers=1)
 
     # prepare RBM
-    rbm = RBM(rbm_params['nv'], 
+    rbm = RBM(rbm_params['nv'],
               rbm_params['nh'],
               rbm_params['batch_size'])
+    rbm.initialize()
 
     for batch in train_iter:
         # for now only works with batch_size=1
@@ -45,7 +46,7 @@ def train_mnist(batch_size=1, learning_rate=0.1):
 
         # make it flat
         imgs = imgs.reshape((imgs.shape[0], -1))
-       
+
         # for now pass it into RBM one by one, since it doesn't support batch yet
         for img in imgs:
             rbm.train_step(im, learning_rate)
